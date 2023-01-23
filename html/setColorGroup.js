892930de0aeb190efde47ec0a460486fe4b419f5
voy74656 <voyager798035795@gmail.com>
2023-01-23 14:36:14 +0800

    markdown格式修改

--- html/setColorGroup.js
+++ html/setColorGroup.js
@@ -1,58 +1,27 @@
-function getUrlParam(paraName) {
-	// 获取当前的url地址,这样我们便可以拿到这个参数.
-	var url = document.location.toString();
-    //alert(url);
-    // 通过"?"对url进行切割,?后面的便是参数了
-    var arrObj = url.split("?");
-    //orrObj是一个数组,数组的0索引是url地址,1索引便是参数的名字和值了,如(id=1)
-    if (arrObj.length > 1) {
-    // 如果有传递过来参数,我们便使用"&"继续将参数进行切割如(id=1&name=abc)
-        var arrPara = arrObj[1].split("&");
-        var arr;
-    // 如果有多个参数就遍历参数的数组    
-        for (var i = 0; i < arrPara.length; i++) {
-        //我们可以看到每个参数的名字和值是以"="分割的,那么我们可以继续切割然后得到参数的名字和对应的值
-            arr = arrPara[i].split("=");
-            // 如果这个参数的名字我们想要的参数,就返回这个参数的值
-            if (arr != null && arr[0] == paraName) {
-                return arr[1];
-            }
-        }
-        // 否则就返回"",表示地址中有参数,但是没有我们想要的
-        return "";
-    }
-    else {
-	    // 返回"" 表示当前地址只是一个简单的url地址,并不携带参数
-        return "";
-    }
+function _setPoperty(_var, _value) {
+    document.documentElement.style.setProperty(_var, _value)
 }
 
-function _set_style(regular, thin, thinner, dark){
-    document.documentElement.style.setProperty('--c-regular', regular)
-    document.documentElement.style.setProperty('--c-thin', thin)
-    document.documentElement.style.setProperty('--c-thinner', thinner)
-    document.documentElement.style.setProperty('--c-dark', dark)
-    
+function _set_style(regular, c01, c02, c03, cp, cpp) {
+    _setPoperty('--cc-regular', regular)
+    _setPoperty('--cc-01', c01)
+    _setPoperty('--cc-02', c02)
+    _setPoperty('--cc-03', c03)
+    _setPoperty('--cc-p', cp)
+    _setPoperty('--cc-pp', cpp)
 }
-function setColorGroup(){
-    // element = getUrlParam('element')
+
+function setColorGroup() {
     element = document.getElementById("element").innerHTML
     switch (element) {
-        case "pyro": _set_style('#ec4923', '#f67759', '#f69982', '#99270b'); break; //火
-        case "hydro": _set_style('#00BFFF', '#40cfff', '#73dcff', '#007ca6'); break;  //水
-        case "anemo": _set_style('359697', '#68cbcb', '#83cbcb', '#116262'); break;  //风
-        case "electro": _set_style('#945DC4', '#b889e2', '#c3a0e2', '#521e7f'); break; //雷
-        case "dendro": _set_style('#66AD16', '#95d64a', '#a6d66f', '#407007'); break; //草
-        case "cryo": _set_style('#4682B4', '#76acda', '#90b8da', '#174a75'); break; //冰
-
-        case "geo": _set_style('#DEBD6C', '#efd493', '#efdbab', '#907123'); break; //岩
-        case "火": _set_style('#ec4923', '#f67759', '#f69982', '#99270b'); break; //火
-        case "水": _set_style('#00BFFF', '#40cfff', '#73dcff', '#007ca6'); break;  //水
-        case "风": _set_style('359697', '#68cbcb', '#83cbcb', '#116262'); break;  //风
-        case "雷": _set_style('#945DC4', '#b889e2', '#c3a0e2', '#521e7f'); break; //雷
-        case "草": _set_style('#66AD16', '#95d64a', '#a6d66f', '#407007'); break; //草
-        case "冰": _set_style('#4682B4', '#76acda', '#90b8da', '#174a75'); break; //冰
-        case "岩": _set_style('#DEBD6C', '#efd493', '#efdbab', '#907123'); break; //岩
-        default: _set_style('#171717','#6b6b6b','#717171','#000000');
-    }
+        case "火": _set_style('#B8584B', '#F1DEDB', '#E3BCB7', '#D49B93', '#8B4137', '#5D2B25'); break; //火
+        case "水": _set_style('#518ABB', '#DCE8F1', '#B9D0E4', '#97B9D6', '#386891', '#264560'); break;  //水
+        case "风": _set_style('#378383', '#D1ECEC', '#A4D9D9', '#76C6C6', '#296262', '#1B4242'); break;  //风
+        case "岩": _set_style('#C09257', '#F2E9DD', '#E6D3BC', '#D9BE9A', '#986F39', '#654A26'); break; //岩
+        case "雷": _set_style('#6455A6', '#E0DDED', '#C1BADC', '#A298CA', '#4B407D', '#322A53'); break; //雷
+        case "冰": _set_style('#68BFD8', '#E1F2F7', '#C3E5EF', '#A4D9E8', '#319FBF', '#216A7F'); break; //冰
+        case "草": _set_style('#6D9840', '#E2EED6', '#C5DCAC', '#A8CB83', '#527230', '#364C20'); break; //草
+        default: _set_style('#44546A', '#D6DCE5', '#ADB9CA', '#8497B0', '#333F50', '#222A35'); // default blackblue
 }
+}
+
